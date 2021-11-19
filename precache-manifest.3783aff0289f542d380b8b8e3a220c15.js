20fb9d3f9ca4f48546bac07f8d773c23036f7b37
Ben <j1712032377@163.com>
2021-11-19 20:20:38 +0800

    deploy

---
+++ precache-manifest.3783aff0289f542d380b8b8e3a220c15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf0980d217a6205c0e9",
+    "url": "/money-note-website/css/app.8c4b15a1.css"
+  },
+  {
+    "revision": "4196b9ecb04daa6ba11448905492e106",
+    "url": "/money-note-website/index.html"
+  },
+  {
+    "revision": "aaf0980d217a6205c0e9",
+    "url": "/money-note-website/js/app.04185382.js"
+  },
+  {
+    "revision": "a4c6a9ec2d401034c514",
+    "url": "/money-note-website/js/chunk-vendors.73460cbb.js"
+  },
+  {
+    "revision": "2550f06097840ca7dbd26b093fdf4185",
+    "url": "/money-note-website/manifest.json"
+  },
+  {
+    "revision": "58cee59c83387355c316bd75d930a567",
+    "url": "/money-note-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-note-website/robots.txt"
+  }
+]);
